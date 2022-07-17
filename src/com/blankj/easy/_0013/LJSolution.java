ed550ecdbec9c8bd1a99d5401a4c77d0b671ebee
luojing <luojing@bhfae.com>
2022-07-17 22:36:20 +0800

    update 0013 solution

--- src/com/blankj/easy/_0013/LJSolution.java
+++ src/com/blankj/easy/_0013/LJSolution.java
@@ -10,7 +10,15 @@ import java.util.Map;
  */
 
 public class LJSolution {
+    /**
+     * 罗马数字的逻辑
+     * 0. I=1、V=5、X=10、L=50、C=100、D=500、M=1000；
+     * 1. 相同数字连写，所表示的数等于这些数字相加得到的数，如III=3；
+     * 2. 小的数字在大的数字右边，所表示的数等于这些数字相加得到的数，如VIII=8、XII=12；
+     * 3. 小的数字(仅限I=1、X=10和C=100)在大的数字左边，所表示的数等于大数减小数得到的数，如IV=4、IX=9；
+     */
     public int romanToInt(String s) {
+        // 构建罗马数字和阿拉伯数字的映射关系，并存入映射表
         Map<Character, Integer> map = new HashMap<>();
         map.put('I', 1);
         map.put('V', 5);
@@ -19,12 +27,17 @@ public class LJSolution {
         map.put('C', 100);
         map.put('D', 500);
         map.put('M', 1000);
+        // 获取罗马数字字符串的长度
         int len = s.length();
+        // 初始化sum，并将最后一个罗马数字值的映射阿拉伯数字赋值给sum
         int sum = map.get(s.charAt(len -1));
+        // 从倒数第二个罗马数字开始，到第一个罗马数字结束，遍历，注意--i，先-1再引用
         for (int i = len - 2; i >= 0 ; --i) {
             if (map.get(s.charAt(i)) < map.get(s.charAt(i + 1))) {
+                // 如果左边的[s.charAt(i)]小于右边的[s.charAt(i+1)]，sum - 左边的
                 sum -= map.get(s.charAt(i));
             } else {
+                // 如果左边的[s.charAt(i)]大右边的[s.charAt(i+1)]，sum + 左边的
                 sum += map.get(s.charAt(i));
             }
         }
